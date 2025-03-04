19ef2776c93d36c9065002bc9158679c2034305b
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-03-04 21:33:26 +0200

    Spring-Fundamentals-Gira

---
+++ src/main/java/bg/softuni/gira/controller/HomeController.java
@@ -0,0 +1,49 @@
+package bg.softuni.gira.controller;
+
+import bg.softuni.gira.config.UserSession;
+import bg.softuni.gira.dto.TaskInfoDto;
+import bg.softuni.gira.entity.User;
+import bg.softuni.gira.service.TaskService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class HomeController {
+
+    private final UserSession userSession;
+    private final TaskService taskService;
+
+    public HomeController(UserSession userSession, TaskService taskService) {
+        this.userSession = userSession;
+        this.taskService = taskService;
+    }
+
+
+    @GetMapping("/")
+    public String nonLoggedIndex() {
+        if(userSession.isLoggedIn()) {
+            return "redirect:/home";
+        }
+
+        return "index";
+    }
+
+    @GetMapping("/home")
+    @Transactional
+    public String loggedInIndex(Model model) {
+        if(!userSession.isLoggedIn()) {
+            return "redirect:/";
+        }
+        List<TaskInfoDto> allTasks = taskService.listTasks().stream().map(TaskInfoDto::new).toList();
+
+        model.addAttribute("allTasks", allTasks);
+
+        return "home";
+    }
+}
