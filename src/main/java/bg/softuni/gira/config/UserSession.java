16cfd3387c2cbfec8e43a2433436271f3308bef0
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-03-08 13:06:21 +0200

    Spring-Fundamentals-Gira

---
+++ src/main/java/bg/softuni/gira/config/UserSession.java
@@ -0,0 +1,34 @@
+package bg.softuni.gira.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@SessionScope
+@Component
+public class UserSession {
+
+     private long id;
+     private String username;
+
+     public void login(long id, String username) {
+        this.id = id;
+        this.username = username;
+     }
+
+     public boolean isLoggedIn() {
+         return id > 0;
+     }
+
+    public Long id() {
+         return id;
+    }
+
+    public String username() {
+         return username;
+    }
+
+    public void logout() {
+         id = 0;
+         username = null;
+    }
+}
