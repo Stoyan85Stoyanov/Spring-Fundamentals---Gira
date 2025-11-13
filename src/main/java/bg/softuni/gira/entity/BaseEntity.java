73b6f26a85bd8875aeb98ff86dce8dc2c62c21ac
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-13 19:22:49 +0200

    Spring-Fundamentals-Gira

--- src/main/java/bg/softuni/gira/entity/BaseEntity.java
+++ src/main/java/bg/softuni/gira/entity/BaseEntity.java
@@ -4,8 +4,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
 
     @Id
@@ -14,13 +18,5 @@ public class BaseEntity {
 
 
     protected BaseEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 }
