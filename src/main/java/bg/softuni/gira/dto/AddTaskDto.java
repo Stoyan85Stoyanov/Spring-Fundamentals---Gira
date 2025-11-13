b64a46786f1eebe1a0f1b944a8f3525b9fcee098
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-13 19:22:45 +0200

    Spring-Fundamentals-Gira

--- src/main/java/bg/softuni/gira/dto/AddTaskDto.java
+++ src/main/java/bg/softuni/gira/dto/AddTaskDto.java
@@ -4,9 +4,11 @@ import bg.softuni.gira.entity.enums.ClassificationName;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class AddTaskDto {
 
     @NotBlank
@@ -23,38 +25,4 @@ public class AddTaskDto {
     @NotNull
     private ClassificationName classification;
 
-    public AddTaskDto() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public ClassificationName getClassification() {
-        return classification;
-    }
-
-    public void setClassification( ClassificationName classification) {
-        this.classification = classification;
-    }
 }
