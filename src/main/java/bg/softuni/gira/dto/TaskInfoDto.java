fe4764d5e10487f356d8db26696587c4abedea32
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-14 21:54:23 +0200

    Spring-Fundamentals-Gira

--- src/main/java/bg/softuni/gira/dto/TaskInfoDto.java
+++ src/main/java/bg/softuni/gira/dto/TaskInfoDto.java
@@ -4,9 +4,11 @@ import bg.softuni.gira.entity.Task;
 import bg.softuni.gira.entity.User;
 import bg.softuni.gira.entity.enums.ClassificationName;
 import bg.softuni.gira.entity.enums.Progress;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class TaskInfoDto {
 
     private Long id;
@@ -27,69 +29,5 @@ public class TaskInfoDto {
         this.progress = task.getProgress();
         this.classificationName = task.getClassification().getName();
         this.user = task.getUser();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public String getClassification() {
-        return classification;
-    }
-
-    public void setClassification(String classification) {
-        this.classification = classification;
-    }
-
-    public Progress getProgress() {
-        return progress;
-    }
-
-    public void setProgress(Progress progress) {
-        this.progress = progress;
-    }
-
-    public ClassificationName getClassificationName() {
-        return classificationName;
-    }
-
-    public void setClassificationName(ClassificationName classificationName) {
-        this.classificationName = classificationName;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 }
