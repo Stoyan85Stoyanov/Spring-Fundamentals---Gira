21c65a1a599aaccb37cf336fbcbb028fdf169c27
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-15 11:30:12 +0200

    Spring-Fundamentals-Gira

--- src/main/java/bg/softuni/gira/repository/UserRepository.java
+++ src/main/java/bg/softuni/gira/repository/UserRepository.java
@@ -11,7 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameOrEmail(String username, String email);
 
-//    Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
 }
