008f7970c029744544a8b826ddf32056c5681722
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-15 11:30:04 +0200

    Spring-Fundamentals-Gira

--- src/main/java/bg/softuni/gira/dto/UserLoginDto.java
+++ src/main/java/bg/softuni/gira/dto/UserLoginDto.java
@@ -3,7 +3,9 @@ package bg.softuni.gira.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class UserLoginDto {
 
     @NotBlank
@@ -13,25 +15,5 @@ public class UserLoginDto {
     @NotBlank
     @Size(min = 3, max = 20)
     private String password;
-
-
-    public UserLoginDto() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
